bcf900b0ee26b5db447b8daf6d8c9fc3cbc48c0e
Павел Курлыков <paulkurlykov@gmail.com>
2024-12-09 22:08:18 +0300

    add getone controller

--- controllers/questionController.js
+++ controllers/questionController.js
@@ -1,22 +1,25 @@
 const Questions = require('../models/questions');
+const ApiErrors = require('../error/ApiErrors');
 
 const create = async (req, res) => {
 
 try {
     const {question, label, answerType, options, textAnswer, codeSnippet} = req.body;
 
-    const questionItem = new Questions({
-        question,
-        label,
-        answerType,
-        options,
-        textAnswer,
-        codeSnippet
-    });
+    // const questionItem = new Questions({
+    //     question,
+    //     label,
+    //     answerType,
+    //     options,
+    //     textAnswer,
+    //     codeSnippet
+    // });
 
-    const responce = await questionItem.save();
+    // const responce = await questionItem.save();
 
-    return res.json(responce);
+    const response = {question, label, answerType, options, textAnswer, codeSnippet};
+
+    return res.json(response);
 } catch (err) {
     console.error("Exception " + err);
 }
@@ -27,7 +30,9 @@ try {
 const getAll = async (req, res) => {
     try {
         console.log('inside get-request-function!')
-        const questions = await Questions.find({});
+        // const questions = await Questions.find({});
+
+        const questions = [];
 
         if(questions.length === 0) {
             return res.json({message: 'there is no data'});
@@ -39,8 +44,18 @@ const getAll = async (req, res) => {
     }
 };
 
-const getOne = async (req, res) => {
-    let it = "be";
+const getOne = async (req, res, next) => {
+
+
+    try {
+        const {id} = req.params;
+
+        const question = await Questions.findById(id).exec();
+    
+        return res.json(question);
+    } catch (err) {
+        next(ApiErrors.badRequest('Could not get question by ID'));
+    }
 };
 
 module.exports = { create, getAll, getOne };
