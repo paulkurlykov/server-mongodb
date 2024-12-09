41611f939b810c702b0f23ba6655ad2f84de1f6a
Павел Курлыков <paulkurlykov@gmail.com>
2024-12-09 11:09:51 +0300

    testing redirect

--- server.js
+++ server.js
@@ -1,33 +1,37 @@
 const express = require("express");
-const mongoose = require('mongoose');
-const handlebars = require('express-handlebars');
-require('dotenv').config();
-const cors = require('cors');
-const router = require('./routers/index');
-const errorHandler = require('./middlewares/errorHandlingMiddleware');
+const mongoose = require("mongoose");
+const handlebars = require("express-handlebars");
+require("dotenv").config();
+const cors = require("cors");
+const router = require("./routers/index");
+const errorHandler = require("./middlewares/errorHandlingMiddleware");
 
-
-
-
-
-
-const mongoDBKey = `mongodb+srv://${process.env.MONGO_LOGIN}:${process.env.MONGO_PASSWORD}@quiz-db.4qrs7.mongodb.net/?retryWrites=true&w=majority&appName=Quiz-DB`
+const mongoDBKey = `mongodb+srv://${process.env.MONGO_LOGIN}:${process.env.MONGO_PASSWORD}@quiz-db.4qrs7.mongodb.net/?retryWrites=true&w=majority&appName=Quiz-DB`;
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use('/api', router);
+app.use("/api", router);
 // const hbs = handlebars.create({
 //     defaultLayout: "main",
 //     extname: "hbs"
 // })
+
+app.use((req, res, next) => {
+    console.log(`Метод: ${req.method}`);
+    console.log(`Заголовки: ${JSON.stringify(req.headers)}`);
+
+    if (req.method === "POST") {
+        console.log("POST-запрос был перенаправлен.");
+    }
+    next();
+});
 app.use(errorHandler);
 
 // app.engine("hbs", hbs.engine);
 // app.set("view engine", "hbs");
 // app.set("views", "views");
-
 
 const start = async () => {
     try {
