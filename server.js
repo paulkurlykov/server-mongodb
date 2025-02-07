040c2ed1b7b89c0ef1bd1406e229d694f74c29c1
Павел Курлыков <paulkurlykov@gmail.com>
2025-02-07 14:49:17 +0300

    add path

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const mongoose = require("mongoose");
 const handlebars = require("express-handlebars");
 require("dotenv").config();
